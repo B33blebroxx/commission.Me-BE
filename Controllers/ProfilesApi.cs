10b46c0ea0c004840b50418acf0ce50b09f3b373
B33blebroxx <b33blebroxx@gmail.com>
2024-07-09 15:51:47 -0500

    Added and tested Get call for all profiles

---
+++ Controllers/ProfilesApi.cs
@@ -0,0 +1,19 @@
+﻿namespace CommissionMe.Controllers
+{
+    public static class ProfilesApi
+    {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/profiles", (CommissionMeDbContext db) =>
+            {
+                var profiles = db.Profiles;
+                if (profiles == null)
+                {
+                    return (Results.NotFound());
+                }
+
+                return Results.Ok(profiles);
+            });
+        }
+    }
+}
