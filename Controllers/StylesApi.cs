9889c6ccfe163c5c1dd0c98b56ce74feed58604a
B33blebroxx <b33blebroxx@gmail.com>
2024-07-24 10:55:15 -0500

    Added controllers an calls for styles, tags, and started on PostTags controller

---
+++ Controllers/StylesApi.cs
@@ -0,0 +1,42 @@
+﻿using CommissionMe.Models;
+namespace CommissionMe.Controllers
+{
+    public static class StylesApi
+    {
+        public static void Map(WebApplication app)
+        {
+            //Get all styles
+            app.MapGet("/styles", (CommissionMeDbContext db) =>
+            {
+                var styles = db.Styles;
+                if (styles == null)
+                {
+                    return (Results.NotFound());
+                }
+
+                return Results.Ok(styles);
+            });
+
+            //Add a style
+            app.MapPost("/styles", (CommissionMeDbContext db, Style style) =>
+            {
+                db.Styles.Add(style);
+                db.SaveChanges();
+                return Results.Created($"/styles/{style.Id}", style);
+            });
+
+            //Delete a style
+            app.MapDelete("/styles/{id}", (CommissionMeDbContext db, int id) =>
+            {
+                var style = db.Styles.SingleOrDefault(s => s.Id == id);
+                if (style == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Styles.Remove(style);
+                db.SaveChanges();
+                return Results.Ok();
+            });
+        }
+    }
+}
---
+++ Controllers/TagsApi.cs
@@ -0,0 +1,41 @@
+﻿namespace CommissionMe.Controllers
+{
+    public static class TagsApi
+    {
+        public static void Map(WebApplication app)
+        {
+            //Get all tags
+            app.MapGet("/tags", (CommissionMeDbContext db) =>
+            {
+                var tags = db.Tags;
+                if (tags == null)
+                {
+                    return (Results.NotFound());
+                }
+
+                return Results.Ok(tags);
+            });
+
+            //Add a tag
+            app.MapPost("/tags", (CommissionMeDbContext db, Tag tag) =>
+            {
+                db.Tags.Add(tag);
+                db.SaveChanges();
+                return Results.Created($"/tags/{tag.Id}", tag);
+            });
+
+            //Delete a tag
+            app.MapDelete("/tags/{id}", (CommissionMeDbContext db, int id) =>
+            {
+                var tag = db.Tags.SingleOrDefault(t => t.Id == id);
+                if (tag == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Tags.Remove(tag);
+                db.SaveChanges();
+                return Results.Ok();
+            });
+        }
+    }
+}
